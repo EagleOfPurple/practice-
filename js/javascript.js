09f8cd1264087400a3945e65c135866aeba3e6cb
Eagle <eagleofpurple@gmail.com>
2020-06-18 20:21:43 +0300

    owl carusel added

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,21 @@
 $(document).ready(function() {
             $('#carouselOne').owlCarousel({
                 loop:true,
-                margin:50,
+                margin: 30,
                 nav: false,
-                autoplay:true,
-                smartSpeed:1000, 
-                autoplayTimeout:2000,
-                responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
-                    0:{
-                        items:1
-                    },
-                    600:{
-                        items:2
-                    },
-                    1000:{
-                        items:4
-                    }
-                }
+//                autoplay:true,
+//                smartSpeed:1000, 
+//                autoplayTimeout:2000,
+//                responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+//                    0:{
+//                        items:1
+//                    },
+//                    600:{
+//                        items:2
+//                    },
+//                    1000:{
+//                        items:4
+//                    }
+//                }
             });
         });
