365c7e52df938860c0c7af0345d8fd07da4e40a0
Eagle <eagleofpurple@gmail.com>
2020-06-18 18:21:25 +0300

    style interactive detailes added

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+            $('#carouselOne').owlCarousel({
+                loop:true,
+                margin:50,
+                nav: false,
+                autoplay:true,
+                smartSpeed:1000, 
+                autoplayTimeout:2000,
+                responsive:{ //Адаптивность. Кол-во выводимых элементов при определенной ширине.
+                    0:{
+                        items:1
+                    },
+                    600:{
+                        items:2
+                    },
+                    1000:{
+                        items:4
+                    }
+                }
+            });
+        });
